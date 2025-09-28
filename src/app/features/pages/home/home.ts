f45b795d925d797e3dacd7c3e61142c2f07dabbb
Tatiana Murillo <tatianamurillomosquera1@gmail.com>
2025-09-27 19:06:55 -0500

    implementación básica del header y home

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,34 +1,20 @@
-import { Component,inject} from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
+import { Component,signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { Auth } from "../../../shared/services/auth"
+import {Book} from '../../../shared/interfaces/book'
+
+export type BookShelfItem = Pick<Book, 'name' | 'pages' | 'pages_read' | 'rating' | 'portrait_url'>;
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   imports: [
-    RouterLink,
     FormsModule
   ],
   styleUrl: './home.css'
 })
 export class Home {
-
-  authService = inject(Auth);
-  router = inject(Router);
-
-  onProfile() {
-    this.router.navigate(['/profile']);
-  }
-  onCollection(){}
-
-  onBorrow(){}
-
-  onSearch(){}
-  onLogout(){
-    this.authService.logout();
-    this.router.navigateByUrl('')
-  }
+  bookshelvesItems = signal<BookShelfItem[]>([]);
 
 }
+
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,14 +1,33 @@
-import { Component, inject } from "@angular/core";
-import { Auth } from "../../services/auth";
+import { Component,inject} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import { Auth } from "../../services/auth"
+
 
 @Component({
   selector:'app-header',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl:'./header.html',
   styleUrl:'./header.css'
 })
 export class Header {
   authService = inject(Auth);
+  router = inject(Router);
   isLogged = this.authService.isLogged;
+
+  onProfile() {
+    this.router.navigate(['/profile']);
+  }
+  onCollection(){}
+
+  onBorrow(){}
+
+  onSearch(){}
+  onLogout(){
+    this.authService.logout();
+    this.router.navigateByUrl('')
+  }
+
 
 }
