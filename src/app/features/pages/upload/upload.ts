ea85026f727d6cd7dcc5b6a9f350221d1255a3b5
Tatiana Murillo <tatianamurillomosquera1@gmail.com>
2025-09-28 22:09:23 -0500

    Implementación del componente upload

--- src/app/features/pages/upload/upload.ts
+++ src/app/features/pages/upload/upload.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {BookInterface} from '../../../shared/interfaces/book-interface';
+import {BooksService} from '../../../shared/services/books-service';
+
+
 
 @Component({
   selector: 'app-upload',
-  imports: [],
   templateUrl: './upload.html',
-  styleUrl: './upload.css'
+  imports: [
+    FormsModule
+  ],
+  styleUrls: ['./upload.css']
 })
-export class Upload {
+export class Upload{
+  title : string = 'Agregar libros'
+  showPopup = false;
+  selectedFile: File | null = null;
+  searchTerm: string = '';
+  books: BookInterface[] = [];
+  filteredBooks: BookInterface[] = [];
 
-  title : string = 'Agregar libro';
+  constructor(
+    private bookService: BooksService,
+    private router: Router,
+    private eRef: ElementRef
+  ) {
+    this.books = this.bookService.getBooks();
+  }
 
+  // Evento cuando selecciona un archivo
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      this.showPopup = true;
+    }
+  }
 
+  onSearch() {
+    this.filteredBooks = this.books.filter(book =>
+      book.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  goToBook(book: BookInterface) {
+    this.router.navigate(['/book', book.id]);
+    this.filteredBooks = [];
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.filteredBooks = [];
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  handleEscape() {
+    this.filteredBooks = [];
+  }
 }
