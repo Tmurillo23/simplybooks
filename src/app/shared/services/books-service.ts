cddd2caf6380844ab538c5b990e8c397c09fc237
Tatiana Murillo <tatianamurillomosquera1@gmail.com>
2025-10-02 16:39:09 -0500

    Cambios en el diseño y refactor

--- src/app/shared/services/books-service.ts
+++ src/app/shared/services/books-service.ts
@@ -6,10 +6,10 @@ import {BookInterface} from '../interfaces/book-interface';
 })
 export class BooksService {
   private books: BookInterface[] = [
-    { id: 1, title: 'Don Quijote de la Mancha', author: 'Miguel de Cervantes', year: 1605, portrait_url:'src/app/temporal_assets/600_9789587433913.jpg' , pages:1100 },
-    { id: 2, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', year: 1967, portrait_url :'src/app/temporal_assets/portada_cien_anos_de_soledad_0.jpg', pages : 471 },
-    { id: 3, title: 'La Ciudad y los Perros', author: 'Mario Vargas Llosa', year: 1963, portrait_url:'src/app/temporal_assets/la_ciudad_y_los_perros.jpg', pages: 448 },
-    { id: 4, title: 'Rayuela', author: 'Julio Cortázar', year: 1963, portrait_url: 'src/app/temporal_assets/Rayuela_JC.png', pages : 730 }
+    { id: 1, title: 'Don Quijote de la Mancha', author: 'Miguel de Cervantes', year: 1605, portrait_url:'public/temporal_assets/600_9789587433913.jpg' , pages:1100 },
+    { id: 2, title: 'Cien Años de Soledad', author: 'Gabriel García Márquez', year: 1967, portrait_url :'public/temporal_assets/portada_cien_anos_de_soledad_0.jpg', pages : 471 },
+    { id: 3, title: 'La Ciudad y los Perros', author: 'Mario Vargas Llosa', year: 1963, portrait_url:'public/temporal_assets/la_ciudad_y_los_perros.jpg', pages: 448 },
+    { id: 4, title: 'Rayuela', author: 'Julio Cortázar', year: 1963, portrait_url: 'public/temporal_assets/Rayuela_JC.png', pages : 730 }
   ];
 
   getBooks(): BookInterface[] {
