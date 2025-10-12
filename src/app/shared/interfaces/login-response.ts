5072280c120220fad3a9eb9d9e775b0b1f2331a3
Sebastián T. <sebas65252@gmail.com>
2025-10-12 17:31:35 -0500

    Correcion carpetas

--- src/app/shared/interfaces/login-response.ts
+++
@@ -1,8 +0,0 @@
-export interface LoginResponse {
-  success:boolean;
-  message?:string;
-  redirectTo?:string;
-}
-
-export interface SignUpResponse extends LoginResponse{}
-export interface ResetPasswordResponse extends LoginResponse{}
--- src/environments/environment.dev.ts
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_KEY = 'xxx';
-export const SUPABASE_URL = 'xxx';
-export const SUPABASE_FILES_BUCKET = 'xxx';
